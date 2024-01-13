939ecad7f03da34dfa4cb807775199caf05210c5
jumpre <skyblueyoshisaur@gmail.com>
2024-01-13 12:03:00 +0000

    started on 'isReservered' method

--- src/BrainfuckAss.java
+++ src/BrainfuckAss.java
@@ -1,2 +1,23 @@
 public class BrainfuckAss {
+
+    private static boolean isReservered(char c)
+    {
+        switch(c){
+            case '[':
+            case ']':
+            case '+':
+            case '-':
+            case '.':
+            case ',':
+            case ':':
+            case ';':
+            case '|':
+            case '@': //10th reserved character
+            case '"':
+            case '!':
+            case '':
+
+
+        }
+    }
 }
